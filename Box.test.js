e6d2d888480dae3f53e8767f3b8c8ac003d672c1
Christiaan Westerbeek <chris@devotis.nl>
2017-10-20 09:48:06 +0200

    Add test for Box

--- Box.test.js
+++ Box.test.js
@@ -1,5 +1,16 @@
 import Box from './Box';
 
 describe('Box', () => {
+  test('map and fold', () => {
+    const x = 2;
+    const f = n => n * n;
+    const g = n => n + 1;
 
+    expect(
+      Box(x)
+      .map(f)
+      .map(g)
+      .fold(a => a),
+    ).toEqual(5);
+  });
 });
